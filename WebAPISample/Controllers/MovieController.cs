020da727dbeec005d75cf7b4f9bd6da72bff24bb
Adam <dev.adammilbrath@gmail.com>
2020-01-13 14:24:10 -0600

    Created Delete and updated GET

--- WebAPISample/Controllers/MovieController.cs
+++ WebAPISample/Controllers/MovieController.cs
@@ -13,45 +13,67 @@ namespace WebAPISample.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET api/values
-        public IEnumerable<string> Get() // Retrieve all movies from db logic
+        public IHttpActionResult Get() // Retrieve all movies from db logic
         {
-
-            return new string[] { "movie1 string", "movie2 string" };
+            List<Movie> movies = new List<Movie>();
+            foreach (var movie in db.Movies)
+            {
+                movies.Add(movie);
+            }
+            return Ok(movies);
         }
 
         // GET api/values/5
-        public string Get(int id) // Retrieve movie by id from db logic
-        { 
-            Movie movie = db.Movies.Find(id);
-           
-            return "value";
+        public IHttpActionResult Get(int id) // Retrieve movie by id from db logic
+        {
+            var movie = db.Movies.Find(id);
+
+            return Ok(movie);
         }
 
         // POST api/values
-        public void Post([FromBody]Movie movie) // Create movie in db logic
+        public IHttpActionResult Post([FromBody]Movie movie) // Create movie in db logic
         {  
-            if (ModelState.IsValid)
-            {
-                db.Movies.Add(movie);
-                db.SaveChanges();
-                Ok();
-            }
+            //if (ModelState.IsValid)
+            //{
+            //    db.Movies.Add(movie);
+            //    db.SaveChanges();
+                
+            //}
+            return Ok();
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value) // Update movie in db logic
+        public IHttpActionResult Put(int id, [FromBody]string value) // Update movie in db logic
         {
 
 
-
+            return Ok();
         }
 
         // DELETE api/values/5
-        public void Delete(int id) // Delete movie from db logic
+        public IHttpActionResult Delete(int id) // Delete movie from db logic
         {
-            Movie movie = db.Movies.Find(id);
-            db.Movies.Remove(movie);
-            db.SaveChanges();           
+            {
+                if (id <= 0)
+                    return BadRequest("Not a valid movie id");
+
+                using (var moviedel = new ApplicationDbContext())
+                {
+                    var movie = moviedel.Movies
+                        .Where(s => s.MovieId == id)
+                        .FirstOrDefault();
+
+                    moviedel.Entry(movie).State = System.Data.Entity.EntityState.Deleted;
+                    moviedel.SaveChanges();
+                }
+
+                return Ok();
+            }
+            //Movie movie = db.Movies.Find(id);
+            //db.Movies.Remove(movie);
+            //db.SaveChanges();
+            //return Ok();
         }
     }
 
