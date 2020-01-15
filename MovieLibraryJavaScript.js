ae910146831a23b5257a5b9bef7af7d072804fb6
Elias Holter <eliasholter1@gmail.com>
2020-01-15 11:19:53 -0600

    pulling adam's updated css and html files

--- MovieLibraryJavaScript.js
+++ MovieLibraryJavaScript.js
@@ -20,38 +20,40 @@ $(function (){
         );
     }
 
-    $.ajax({
-        url: 'https://localhost:44352/api/movie',
-        dataType: 'json',
-        type: 'GET',
-        contentType: 'application/json',
-        success: function(movies) {
-            $.each(movies, function(i, movie){
-                $movies.append('<li>Title: ' + movie.Title +'\n Genre: ' + movie.Genre +'\n Director: ' + movie.Director + '</li>');
-            });
-        },
-        error: function(){
-            alert('error loading movies');
-        }
-    });
 
-    $('#add-movie').on('click', function() {
-        var movie = {
-            Title: $Title.val(),
-            Genre: $Genre.val(),
-            Director: $Director.val(),
-        }
+    
+    // $.ajax({
+    //     url: 'https://localhost:44352/api/movie',
+    //     dataType: 'json',
+    //     type: 'GET',
+    //     contentType: 'application/json',
+    //     success: function(movies) {
+    //         $.each(movies, function(i, movie){
+    //             $movies.append('<li>Title: ' + movie.Title +'\n Genre: ' + movie.Genre +'\n Director: ' + movie.Director + '</li>');
+    //         });
+    //     },
+    //     error: function(){
+    //         alert('error loading movies');
+    //     }
+    // });
 
-        $.ajax({
-            url: 'https://localhost:44352/api/movie',
-            type: 'POST',
-            data: movie,
-            success: function(newMovie) {
-                $movies.append('<li>Title: ' + newMovie.Title +'\n Genre: ' + newMovie.Genre +'\n Director: ' + newMovie.Director + '</li>');
-            },
-            error: function() {
-                alert('error saving order');
-            }
-        })
-    });
+    // $('#add-movie').on('click', function() {
+    //     var movie = {
+    //         Title: $Title.val(),
+    //         Genre: $Genre.val(),
+    //         Director: $Director.val(),
+    //     }
+
+    //     $.ajax({
+    //         url: 'https://localhost:44352/api/movie',
+    //         type: 'POST',
+    //         data: movie,
+    //         success: function(newMovie) {
+    //             $movies.append('<li>Title: ' + newMovie.Title +'\n Genre: ' + newMovie.Genre +'\n Director: ' + newMovie.Director + '</li>');
+    //         },
+    //         error: function() {
+    //             alert('error saving order');
+    //         }
+    //     })
+    // });
 })
