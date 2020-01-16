f648d40e5d820abd63bbe2dc9628ace3caff5b8d
Elias Holter <eliasholter1@gmail.com>
2020-01-16 08:31:16 -0600

    POST function is working, however mustache throws error saying 'movieid is undefined'. PUT function is semi-functional, still a work in progress.

--- MovieLibraryJavaScript.js
+++ MovieLibraryJavaScript.js
@@ -46,6 +46,48 @@ $(function (){
             error: function() {
                 alert('error saving order');
             }
-        })
+        });
+    });
+
+    $(document).on('click', '.editMovie', function() {
+        alert("You've been clicked!");
+        var $tr = $(this).closest('tr');
+        $tr.find('input.title').val( $tr.find('td.title') );
+        $tr.find('input.genre').val( $tr.find('td.genre') );
+        $tr.find('input.director').val( $tr.find('td.director') );
+        $tr.find('input.title').addClass('edit');
+        $tr.find('input.genre').addClass('edit');
+        $tr.find('input.director').addClass('edit');
+    });
+
+    $(document).on('click', '.cancelEdit', function() {
+        $(this).closest('tr').removeClass('edit');
+    });
+
+    $(document).on('click', '.saveEdit', function() {
+        var $tr = $(this).closest('tr');
+        var movie = {
+            Title: $tr.find('input.title').val(),
+            Genre: $tr.find('input.genre').val(),
+            Director: $tr.find('input.director').val()
+        };
+
+        $.ajax({
+            url: 'https://localhost:44352/api/movie' + $tr.attr('data-id'),
+            type: 'PUT',
+            data: movie,
+            success: function(newMovie) {
+                $tr.find('td.title').html(movie.title);
+                $tr.find('td.genre').html(movie.genre);
+                $tr.find('td.director').html(movie.director);
+                $tr.find('input.title').removeClass('edit');
+                $tr.find('input.genre').removeClass('edit');
+                $tr.find('input.director').removeClass('edit');
+            },
+            error: function() {
+                alert('error updating order');
+            }
+        });
+
     });
 })
